4b6a78bbaed47063d958b038d84ce3c647c711d6
yahya <yahya@LAPTOP-9T1UIGTO>
2023-10-10 11:52:41 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessement_2_task_2_Langham_Hotel_Mangament
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
